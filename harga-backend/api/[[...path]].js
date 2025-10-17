675977c36832729efd9e7c2e62307529bfe7b77c
nikmatRMT <nikforart@gmail.com>
2025-10-17 21:37:35 +0800

    Delete [[...path]].js

--- harga-backend/api/[[...path]].js
+++
@@ -1,7 +0,0 @@
-// Catch-all API route for Vercel so every /api/* request
-// is handled by our single Express app.
-import serverless from 'serverless-http';
-import app from '../src/server.js';
-
-export default serverless(app);
-
