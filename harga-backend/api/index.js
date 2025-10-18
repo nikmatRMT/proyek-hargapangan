b1e8ced6659d8666976173baae0adb580dc7e790
nikmatRMT <nikforart@gmail.com>
2025-10-18 14:33:40 +0800

    Update index.js

--- harga-backend/api/index.js
+++ harga-backend/api/index.js
@@ -2,4 +2,5 @@
 import serverless from 'serverless-http';
 import app from '../src/server.js';
 
-export default serverless(app);
+// Jangan tunggu event loop kosong (pool MySQL dll.) di serverless
+export default serverless(app, { callbackWaitsForEmptyEventLoop: false });
